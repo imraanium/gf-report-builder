7cd0a1b24beba115510f087f25c2d0b9e6b1b9ff
Imraan Abbasakoor <imraan.abbasakoor@gmail.com>
2024-09-20 21:56:55 +0400

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'import/newline-after-import': ['error', { count: 1 }],
   },
 }
--- src/components/outputForm.jsx
+++ src/components/outputForm.jsx
@@ -1,15 +1,14 @@
-import Form from 'react-bootstrap/Form'
-
+import Form from "react-bootstrap/Form";
 
 const OutputForm = () => {
-    return (
-        <Form id='output-form'>
-        <Form.Group className="mb-3 h-100" controlId="todayLesson">
-          <Form.Label>Report</Form.Label>
-          <Form.Control className='output-form__textarea' as="textarea"/>
-        </Form.Group>
-      </Form>
-    )
-}
+  return (
+    <Form id="output-form">
+      <Form.Group className="mb-3 h-100" controlId="todayLesson">
+        <Form.Label>Report</Form.Label>
+        <Form.Control className="output-form__textarea" as="textarea" />
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default OutputForm;
